02c1a52c1ea4aad97ca1ef33d4dfa5e6155009e4
KANDRADE7 <andradekate@icloud.com>
2025-12-19 17:52:21 +0000

    done

--- StackQueueMini.java
+++ StackQueueMini.java
@@ -1,4 +1,7 @@
-//Katherine Andrade, 12/16/25, pd 6 cp3
+//Katherine Andrade, 12/16/25, pd 6 
+//asks user for input, creating a stack. then, every value in stack is duplicated
+//asks user for input a 2nd time, creating a queue. then, rearranges queue 
+//so all even numbers come before odd numbers whilst perserving order. 
 import java.util.Scanner;
 import java.util.Stack;
 import java.util.Queue;
@@ -6,6 +9,7 @@ import java.util.LinkedList;
 
 class StackQueueMini {
   public static void main(String[] args) {
+    //PART 1 : stack 
     Stack <Integer> stack = new Stack <Integer> ();
     Scanner sheep = new Scanner(System.in);
     for(int i=0; i<5; i++){
@@ -14,11 +18,29 @@ class StackQueueMini {
       stack.push(val);
       sheep.nextLine();
     }//end for loop
-    System.out.println("\nOriginal stack: ");
-    System.out.println("\nbottom of stack " + stack + " top of stack");
+    System.out.println("\nOriginal stack: bottom of stack -> " + stack + " <- top of stack");
 
-    //duplicating
-    //new one to reverse then remove and add 2x
+    duplicate(stack);
+    System.out.println("\nDuplicated stack: bottom of stack -> " + stack + " <- top of stack");
+    System.out.println();
+
+  //PART 2: queue
+  Queue <Integer> queue = new LinkedList <Integer>();
+  System.out.println();
+  for(int i=0; i<10; i++){
+      System.out.print("Enter a number: ");
+      int val = sheep.nextInt();
+      queue.add(val);
+  }//end for loop
+
+  System.out.println("\nOriginal queue: bottom of queue--> " + queue + " <-- top of queue");
+    
+  evenOddQueue(queue);
+  System.out.println("\nNew queue:  bottom of queue --> " + queue + " <-- top of queue");
+  
+}// end main
+
+  public static void duplicate(Stack<Integer> stack){
     Stack <Integer> tempStack = new Stack <Integer>();
     while(!stack.isEmpty()){
       int stored = stack.pop();
@@ -30,42 +52,23 @@ class StackQueueMini {
       stack.push(dupe);
       stack.push(dupe);
     }//end while loop
+  }//end duplicate function
 
-    System.out.println();
-    System.out.println("\nDuplicated stack: ");
-    System.out.print("bottom of stack " + stack + " top of stack");
-    System.out.println();
-
-  //PART 2
-  Queue <Integer> queue = new LinkedList <Integer>();
-  System.out.println();
-  for(int i=0; i<10; i++){
-      System.out.print("Enter a number: ");
-      int val = sheep.nextInt();
-      queue.add(val);
-      sheep.nextLine();
+  public static void evenOddQueue (Queue <Integer> queue){
+    Queue <Integer> tempQ = new LinkedList <Integer>(); 
+    for(int i = 0; i< 10;i++){
+      int value = queue.remove();
+      if(value % 2 == 0){
+        queue.add(value); 
+      } else {
+        tempQ.add(value);  
+      }// end else 
     }//end for loop
 
-    System.out.println("\nOriginal queue: ");
-    System.out.print("bottom of queue " + queue + " top of queue");
-    
-    //sorting for find odd numbers
-  Queue <Integer> tempQ = new LinkedList <Integer>(); 
-  for(int i = 0; i<=10;i++){
-    if(queue.peek() % 2 != 0){
-      int value = queue.remove();
-      tempQ.add(value);  
-    }//end if
-    else
-      queue.add(value);
-    }//end while
-
-    while(tempQ.isEmpty()){
+    while(!tempQ.isEmpty()){
       int thing = tempQ.remove();
       queue.add(thing);
     }//end while
-  System.out.println("\nNew queue: ");
-  System.out.print("bottom of queue " + queue + " top of queue");
-  
-} // end main
+  }// end evenOddQueue function
+
 } // end class
